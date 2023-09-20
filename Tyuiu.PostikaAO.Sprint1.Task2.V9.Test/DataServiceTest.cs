2346a03635572638263c1ce8447e5e1b877eade7
Tyuiu.PostikaAO@outlook.com <a.postika@bk.ru>
2023-09-20 23:59:25 +0300

    Task2

---
+++ Tyuiu.PostikaAO.Sprint1.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PostikaAO.Sprint1.Task2.V9.Lib;
+
+namespace Tyuiu.PostikaAO.Sprint1.Task2.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.CalculateVolumeCircle(x);
+            Assert.AreEqual(33.493333333333333, res);
+        }
+    }
+}
---
+++ Tyuiu.PostikaAO.Sprint1.Task2.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PostikaAO.Sprint1.Task2.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PostikaAO.Sprint1.Task2.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d919b740-e75e-41bb-bb94-0ab5c791a4fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
