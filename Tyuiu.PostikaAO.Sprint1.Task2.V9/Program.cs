d3a04b751f29e2a24f6d394e4e1dfad7977608d9
Tyuiu.PostikaAO@outlook.com <a.postika@bk.ru>
2023-09-20 23:09:10 +0300

    Task9

---
+++ Tyuiu.PostikaAO.Sprint1.Task2.V9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PostikaAO.Sprint1.Task2.V9.Lib;
+
+namespace Tyuiu.PostikaAO.Sprint1.Task2.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Постика А. О. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант 9                                                               *");
+            Console.WriteLine("* Выполнил: Постика Артём Олегович | ИИПб-23-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет примерный объем шара              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            
+            Console.WriteLine(" Радиус шара ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("* Объем шара = " + ds.CalculateVolumeCircle(x));
+
+            Console.ReadLine();
+        }
+    }
+}
